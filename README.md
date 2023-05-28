20fd86ef5288f2213ae1e87f817f64e07fb63c56
NureddinFarzaliyev <nureddin.fa@gmail.com>
2023-05-28 12:50:45 +0400

    all anim done

---
+++ README.md
@@ -0,0 +1,7 @@
+# Meme Toggle Switch
+
+![Toggle Switch GIF](capture.gif)
+
+I've decided to create day/night toggle switch from "Designer vs Developer" meme.
+CSS animations are used in this element.
+I've also created all SVGs using Figma.
--- app.js
+++ app.js
@@ -8,14 +8,29 @@ const day = document.querySelector('.day')
 const cloud1 = document.querySelector('.day-img-1')
 const cloud2 = document.querySelector('.day-img-2')
 
-const night = document.querySelector('.night')
+const star = document.querySelector('.stars')
+
+const label = document.querySelector('.toggle-switch label');
+const sun = window.getComputedStyle(label, ':after');
+
+
+function toggleColors() {
+    circles[0].classList.toggle('dayc1')
+    circles[0].classList.toggle('nightc1')
+
+    circles[1].classList.toggle('dayc2')
+    circles[1].classList.toggle('nightc2')
+
+    circles[2].classList.toggle('dayc3')
+    circles[2].classList.toggle('nightc3')
+
+    circles[3].classList.toggle('dayc4')
+    circles[3].classList.toggle('nightc4')
+}
 
 
 toggle.addEventListener('change', () => {
     console.log('hi')
-
-    // day.classList.toggle('hidden')
-    // night.classList.toggle('hidden')
 
     if(switchState == 'day'){
         console.log('to night')
@@ -26,6 +41,7 @@ toggle.addEventListener('change', () => {
         circles[1].classList.add('circles-to-right')
         circles[2].classList.add('circles-to-right')
 
+        toggleColors()
 
         // Day
         cloud1.classList.add('cloud-1-anim-out')
@@ -33,7 +49,10 @@ toggle.addEventListener('change', () => {
 
         cloud2.classList.add('cloud-2-anim-out')
         cloud2.classList.remove('cloud-2-anim-in')
-        
+    
+        // Night
+        star.classList.add('stars-in')
+        star.classList.remove('stars-out')
 
 
     }
@@ -41,10 +60,13 @@ toggle.addEventListener('change', () => {
         console.log('to day')
         switchState = 'day'
 
+
         // Background
         circles[0].classList.remove('circles-to-right')
         circles[1].classList.remove('circles-to-right')
         circles[2].classList.remove('circles-to-right')
+
+        toggleColors()
 
         // Day
         cloud1.classList.add('cloud-1-anim-in')
@@ -53,6 +75,8 @@ toggle.addEventListener('change', () => {
         cloud2.classList.add('cloud-2-anim-in')
         cloud2.classList.remove('cloud-2-anim-out')
 
+        star.classList.remove('stars-in')
+        star.classList.add('stars-out')
     }
 
 })
